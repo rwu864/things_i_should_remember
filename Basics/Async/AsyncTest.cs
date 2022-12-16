5b47f3b8b2f1587dcc6f5119b562244448e25f4e
raywu1994 <rwu864@gmail.com>
2022-12-15 20:02:14 -0800

    add async

---
+++ Basics/Async/AsyncTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Basics.Async
+{
+    public class AsyncTest
+    {
+        private readonly ITestOutputHelper _console;
+
+        public AsyncTest(ITestOutputHelper console)
+        {
+            _console = console;
+        }
+
+        [Fact]
+        public async Task Test_NonBlocking()
+        {
+            var worker = new Worker(_console);
+
+            // mineOreTask is not awaited
+            var mineOreTask = worker.MineOreAsync(); 
+
+            var cutTreetasks = new List<Task>();
+            for(int i = 0; i < 5; i++)
+            {
+                cutTreetasks.Add(worker.CutTreesAsync());
+            }
+
+            await Task.WhenAll(cutTreetasks);
+            var result = await mineOreTask;
+            Assert.True(result);
+
+            // console result: 
+            //start mining ore...
+            //start cutting tree...
+            //start cutting tree...
+            //start cutting tree...
+            //start cutting tree...
+            //start cutting tree...
+            //tree has been cut
+            //tree has been cut
+            //tree has been cut
+            //tree has been cut
+            //tree has been cut
+            //ore has been mined
+
+            // this takes a total of 10s because the long task starts
+            // but does not block the smaller tasks 
+        }
+
+        [Fact]
+        public async Task Test_Blocking()
+        {
+            var worker = new Worker(_console);
+
+            // mine ore task is awaited right away 
+            var result = await worker.MineOreAsync();
+
+            for (int i = 0; i < 5; i++)
+            {
+                await worker.CutTreesAsync();
+            }
+
+            Assert.True(result);
+
+            // console output: 
+
+            //start mining ore...
+            //ore has been mined
+            //start cutting tree...
+            //tree has been cut
+            //start cutting tree...
+            //tree has been cut
+            //start cutting tree...
+            //tree has been cut
+            //start cutting tree...
+            //tree has been cut
+            //start cutting tree...
+            //tree has been cut...
+
+            // this will take about 15s since we are waiting for the result of each one 
+        }
+    }
+
+   
+}
---
+++ Basics/Async/Worker.cs
@@ -0,0 +1,32 @@
+﻿using Xunit.Abstractions;
+
+namespace Basics.Async
+{
+    public class Worker
+    {
+        private readonly ITestOutputHelper _console;
+
+        public Worker(ITestOutputHelper console)
+        {
+            _console = console;
+        }
+
+        public async Task<bool> MineOreAsync()
+        {
+            // takes 10s to mine ore
+            _console.WriteLine("start mining ore...");
+            await Task.Delay(10 * 1000);
+            _console.WriteLine("ore has been mined");
+            return true;
+        }
+
+        public async Task<bool> CutTreesAsync()
+        {
+            // takes 1s to cut trees
+            _console.WriteLine("start cutting tree...");
+            await Task.Delay(1000);
+            _console.WriteLine("tree has been cut");
+            return true;
+        }
+    }
+}
